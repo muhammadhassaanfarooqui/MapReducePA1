72fd9126e3ae5b3435a618e4519b3e9ba91f8a82
Hassaan <muhammadhassaanfarooqui@gmail.com>
2018-02-23 20:21:37 -0500

    a few fixes

--- task6/reduce.py
+++ task6/reduce.py
@@ -12,7 +12,6 @@ total = 0
 ls = []
 for line in sys.stdin:
 	key, _ = line.strip().split('\t')
-	
 	firstval = key.strip().split(',')
 	if(len(firstval[0]) <= 2):
 		continue
@@ -23,7 +22,10 @@ for line in sys.stdin:
 			ls.append((currentkey, count))
 		count = 1
 		currentkey = key
+
 if(key == currentkey):
+	count = count + 1
 	ls.sort(key=lambda tup: tup[1])
-	culprit = ls[-1:][0]
-	print culprit[0] + '\t' + str(culprit[1])
+	culprits = reversed(ls[-20:])
+	for culprit in culprits:
+		print culprit[0] + '\t' + str(culprit[1])
